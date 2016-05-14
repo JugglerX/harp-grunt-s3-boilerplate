717c75578857170ae4d09804026f081fb4bb2d5c
robaustin <rob.austin@autodesk.com>
2016-05-13 17:04:43 -0700

    s3 grunt deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    aws: grunt.file.readJSON('aws-keys.json'), // Read the file
     dom_munger: {
       your_target: {
         options: {
@@ -11,11 +12,30 @@ module.exports = function(grunt) {
         src: 'www/**/*.html' //could be an array of files
       },
     },
+    aws_s3: {
+      options: {
+          accessKeyId: '<%= aws.AWSAccessKeyId %>',
+          secretAccessKey: '<%= aws.AWSSecretKey %>',
+          region: '<%= aws.AWSRegion %>',
+          uploadConcurrency: 5, // 5 simultaneous uploads
+          downloadConcurrency: 5 // 5 simultaneous downloads
+      },
+      production: {
+          options: {
+              bucket: 'myharpbucket',
+          },
+          files: [
+              {expand: true, cwd: 'www', src: ['**'], dest: '/'}
+          ]
+      },
+    }
   });
 
   grunt.loadNpmTasks('grunt-dom-munger');
+  grunt.loadNpmTasks('grunt-aws-s3');
   // Default task(s).
-  grunt.registerTask('harp-html', ['dom_munger']);
+  grunt.registerTask('harp', ['dom_munger']);
   grunt.registerTask('default', ['dom_munger']);
+  grunt.registerTask('deploy', ['aws_s3']);
 
 };
